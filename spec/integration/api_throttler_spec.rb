6afcc8c04f50f64e3d05dd91a5c9ec1eead055c9
James Shipton <ionysis@gmail.com>
2013-05-27 23:44:02 +0100

    Add integration spec for facebook

---
+++ spec/integration/api_throttler_spec.rb
@@ -0,0 +1,29 @@
+require 'api_throttler'
+
+describe 'Throttle usage to 3rd party APIs' do
+  before(:each) do
+    ApiThrottler::KvStore.kv_store.flushdb
+  end
+
+  context 'When the Facebook rate limit has not been exceeded' do
+    it 'the response should be successful' do
+      $stdout.should_receive(:puts).with('successful facebook response')
+      ApiThrottler.get 'facebook/do_friend'
+    end
+  end
+
+  context 'When the Facebook rate limit has been exceeded' do
+    it 'the response should be successful' do
+      $stdout.should_receive(:puts).with('successful facebook response')
+      increase_api_calls_to_exceed_limit 'facebook'
+      ApiThrottler.get 'facebook/do_friend'
+      sleep 6
+    end
+  end
+
+  def increase_api_calls_to_exceed_limit(service)
+    ((ApiThrottler::Rate.limit[service]+1)).times do
+      ApiThrottler::KvStore.kv_store.setex "#{service}_#{SecureRandom.uuid}", 3, true
+    end
+  end
+end
